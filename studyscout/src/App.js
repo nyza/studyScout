b70f90e385faf1fab3ce0111c961e6e54d4e5f36
nyza <nishiyza@gmail.com>
2021-03-16 14:09:26 -0500

    added study card page

--- studyscout/src/App.js
+++ studyscout/src/App.js
@@ -2,27 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import StudyCard from "./components/Study_Card"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Study Scout!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+       
+       
+        <StudyCard/>
       </header>
-      <AmplifySignOut />
+      {/* <AmplifySignOut /> */}
     </div>
   );
 }
 
-export default withAuthenticator(App);
+// export default withAuthenticator(App);
+export default App;
+
