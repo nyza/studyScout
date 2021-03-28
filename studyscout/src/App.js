c14c156b10700993cc14205a51f7fa2e1fa2212f
jpartin5 <48448340+jpartin5@users.noreply.github.com>
2021-03-28 19:47:10 -0400

    Registration changes to css and js

--- studyscout/src/App.js
+++ studyscout/src/App.js
@@ -6,9 +6,9 @@ import { theme } from './theme';
 import { withAuthenticator} from '@aws-amplify/ui-react'
 import StudyCard from "./StudyCards/Study_Card"
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import { Burger, Menu } from '../src/components';
-
+import Registration from './Registration/Registration'
 
 
 function App() {
@@ -17,11 +17,14 @@ function App() {
   useOnClickOutside(node, () => setOpen(false));
   return (
     
+    
+
     <ThemeProvider theme={theme}>
        <>
         <div className="App">
-          <Router>
         
+          <Router>
+          <Link to="/Registration">Registration</Link>
             <div>
 
             <Navbar />
@@ -31,6 +34,9 @@ function App() {
                 </Route>
                 <Route exact path="/studyCard">
                   <StudyCard />
+                </Route>
+                <Route exact path="/Registration">
+                  <Registration />
                 </Route>
               </Switch>
             </div>
