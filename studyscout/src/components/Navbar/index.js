3b38a143f98e66f8ba39126e380d649ef25d1b08
nward9 <nward9@uncc.edu>
2021-03-30 22:56:02 -0400

    Combined profile page syntax to prepare for demo

--- studyscout/src/components/Navbar/index.js
+++ studyscout/src/components/Navbar/index.js
@@ -28,7 +28,7 @@ const Navbar = () => {
              </NavBtn>
 
              <NavBtn>
-                 <NavBtnLink to="/profilePage"> Profile</NavBtnLink>
+                 <NavBtnLink to="/profile"> Profile</NavBtnLink>
              </NavBtn>
              
          </Nav>   
